94024c82b8e543801697f074505ef65aefeeb3f7
Augustin Šulc <sulc.gusta@gmail.com>
2015-04-27 15:43:44 +0200

    Created Web project. This closes #1

---
+++ src/Server/DeploymentFramework/Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Baud.Deployment.Web.MvcApplication" Language="C#" %>
---
+++ src/Server/DeploymentFramework/Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Baud.Deployment.Web.Startup))]
+
+namespace Baud.Deployment.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
