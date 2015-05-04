89375a93bbfec31b387cb54435a08f9005057e01
Augustin Šulc <sulc.gusta@gmail.com>
2015-05-04 17:28:37 +0200

    Created project for DeployAgent. Closes #12

---
+++ src/Agent/DeployAgent/DeployAgent/AgentService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Baud.Deployment.DeployAgent
+{
+    public class AgentService : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Agent/DeployAgent/DeployAgent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Baud.Deployment.DeployAgent
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<AgentService>();
+            });
+        }
+    }
+}
