d9f3682ab98cb7d22c59455ab8bd8746399eb728
Filip Vondrášek <filip@vondrasek.net>
2015-06-03 13:57:35 +0200

    Added a few screenshot and a magnifier plugin. Updated BXSlider and JQuery plugins.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
 	$('.screenshots').bxSlider({
 		captions: true
 	});
+	
+	$('.image-link').magnificPopup({
+		type: 'image',
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+			duration: 200
+		}
+	});
 });
