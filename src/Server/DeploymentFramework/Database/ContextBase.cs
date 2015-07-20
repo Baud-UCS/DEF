0cb6c7e1093aa0dd5690262cbea16b3bbe063fcc
Augustin Šulc <sulc.gusta@gmail.com>
2015-07-20 17:53:17 +0200

    Entity properies Created, CreatedBy, Updated, UpdatedBy are automatically set by ContextBase. Closes #41

--- src/Server/DeploymentFramework/Database/ContextBase.cs
+++ src/Server/DeploymentFramework/Database/ContextBase.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Baud.Deployment.BusinessLogic.Contracts;
 using Baud.Deployment.Database.Contracts;
 
 namespace Baud.Deployment.Database
 {
     public abstract class ContextBase : DbContext, IDbContext
     {
+        private ICurrentUserProvider _currentUserProvider;
+
+        public ICurrentUserProvider CurrentUserProvider
+        {
+            get
+            {
+                if (_currentUserProvider == null)
+                {
+                    throw new InvalidOperationException("CurrentUserProvider was not set on DbContext.");
+                }
+                return _currentUserProvider;
+            }
+            set
+            {
+                _currentUserProvider = value;
+            }
+        }
+
         public ContextBase()
             : base()
         {
@@ -61,6 +82,77 @@ namespace Baud.Deployment.Database
         {
             var reference = Entry(entity).Reference(navigationProperty);
             reference.CurrentValue = reference.CurrentValue;
+        }
+
+        public override int SaveChanges()
+        {
+            OnSaveChanges();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            OnSaveChanges();
+            return base.SaveChangesAsync();
+        }
+
+        public override Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            OnSaveChanges();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void OnSaveChanges()
+        {
+            var currentUserID = CurrentUserProvider.GetCurrentUserID();
+
+            var stateManager = ((IObjectContextAdapter)this).ObjectContext.ObjectStateManager;
+
+            var createdEntries = stateManager.GetObjectStateEntries(EntityState.Added);
+            foreach (var item in createdEntries)
+            {
+                SetCreatedProperties(item, currentUserID);
+            }
+
+            var changedEntries = stateManager.GetObjectStateEntries(EntityState.Modified);
+            foreach (var item in changedEntries)
+            {
+                SetModifiedProperties(item, currentUserID);
+            }
+        }
+
+        private void SetCreatedProperties(ObjectStateEntry entry, int currentUserID)
+        {
+            var now = DateTime.Now;
+            SetDateTimeValue(entry, "Created", now);
+
+            SetInt32Value(entry, "CreatedBy", currentUserID);
+        }
+
+        private void SetModifiedProperties(ObjectStateEntry entry, int currentUserID)
+        {
+            var now = DateTime.Now;
+            SetDateTimeValue(entry, "Modified", now);
+
+            SetInt32Value(entry, "ModifiedBy", currentUserID);
+        }
+
+        private static void SetInt32Value(ObjectStateEntry entry, string propertyName, int value)
+        {
+            var propertyMetadata = entry.CurrentValues.DataRecordInfo.FieldMetadata.SingleOrDefault(x => x.FieldType.Name == propertyName);
+            if (propertyMetadata.FieldType != null)
+            {
+                entry.CurrentValues.SetInt32(propertyMetadata.Ordinal, value);
+            }
+        }
+
+        private static void SetDateTimeValue(ObjectStateEntry entry, string propertyName, DateTime value)
+        {
+            var propertyMetadata = entry.CurrentValues.DataRecordInfo.FieldMetadata.SingleOrDefault(x => x.FieldType.Name == propertyName);
+            if (propertyMetadata.FieldType != null)
+            {
+                entry.CurrentValues.SetDateTime(propertyMetadata.Ordinal, value);
+            }
         }
     }
 }
--- src/Server/DeploymentFramework/Database/ReflectionDbContextProvider.cs
+++ src/Server/DeploymentFramework/Database/ReflectionDbContextProvider.cs
@@ -4,14 +4,23 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Baud.Deployment.BusinessLogic.Contracts;
 
 namespace Baud.Deployment.Database
 {
-    public class ReflectionDbContextProvider<TDbContext> : NoChangeTrackingDbContextProvider<TDbContext> where TDbContext : DbContext
+    public class ReflectionDbContextProvider<TDbContext> : NoChangeTrackingDbContextProvider<TDbContext> where TDbContext : ContextBase
     {
+        private readonly ICurrentUserProvider _currentUserProvider;
+
+        public ReflectionDbContextProvider(ICurrentUserProvider currentUserProvider)
+        {
+            _currentUserProvider = currentUserProvider;
+        }
+
         protected override TDbContext CreateContextInner()
         {
             var context = Activator.CreateInstance<TDbContext>();
+            context.CurrentUserProvider = _currentUserProvider;
             return context;
         }
     }
